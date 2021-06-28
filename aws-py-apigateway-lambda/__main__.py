f2cd99d464d2f206cb368b3b97142f1445b90526
Tushar Shah <tushar.shah1@gmail.com>
2021-06-28 08:59:50 -0400

    fixed lambda function

--- aws-py-apigateway-lambda/__main__.py
+++ aws-py-apigateway-lambda/__main__.py
@@ -40,18 +40,18 @@ api_lambda_role_policy = iam.RolePolicy('demo-lambda-api-iam-policy',
 
 ######### LAYERS ###########
  
-artifacts_bucket = s3.Bucket('demo-artifacts')
+"""artifacts_bucket = s3.Bucket('demo-artifacts')
 # Upload ffmpeg library to bucket
 api_airtable_layer_zip = s3.BucketObject('demo-hello',
     bucket=artifacts_bucket.id,
     source=pulumi.FileAsset("./step_hello/hello.py")
-)
+)"""
 
 ######## LAMBDAS ###########
 api_airtable = lambda_.Function('demo-api-airtable',
     role=api_lambda_role.arn,
     runtime="python3.8",
-    handler="handler.app",
+    handler="hello.handler",
     #layers=[api_airtable_layer.arn],
     code=pulumi.AssetArchive({
         '.': pulumi.FileArchive('./step_hello')
@@ -59,11 +59,6 @@ api_airtable = lambda_.Function('demo-api-airtable',
     timeout=30,
     memory_size=512,
 )
-
-api_lambda_permission = lambda_.Permission('demo-api-lambda-permission',
-    action="lambda:InvokeFunction",
-    principal="apigateway.amazonaws.com",
-    function=api_airtable.name)
 
 env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
 openapi_spec_template = env.get_template('api.yaml')
@@ -107,6 +102,13 @@ api_gateway_stage = apigateway.Stage('demo-api-gateway-stage',
     opts=pulumi.ResourceOptions(depends_on=[api_gateway])
 )
 
+api_lambda_permission = lambda_.Permission('demo-api-lambda-permission',
+    action="lambda:InvokeFunction",
+    principal="apigateway.amazonaws.com",
+    function=api_airtable.name,
+    source_arn=api_gateway_deployment.execution_arn.apply(lambda arn: arn + "*/*"),
+)
+
 
 pulumi.export("api_lambda_role_name", api_lambda_role.name)
 pulumi.export("api_lambda_role_id", api_lambda_role.id)
@@ -115,9 +117,9 @@ pulumi.export("api_lambda_role_arn", api_lambda_role.arn)
 pulumi.export("api_lambda_role_policy_name", api_lambda_role_policy.name)
 pulumi.export("api_lambda_role_policy_id", api_lambda_role_policy.id)
 # Export the name of the bucket
-pulumi.export('bucket_name', artifacts_bucket.id)
+#pulumi.export('bucket_name', artifacts_bucket.id)
 
-pulumi.export('api_airtable_layer_zip_id__s3_object', api_airtable_layer_zip.id)
+#pulumi.export('api_airtable_layer_zip_id__s3_object', api_airtable_layer_zip.id)
 
 #pulumi.export('api_airtable_layer_arn', api_airtable_layer.arn)
 #pulumi.export('api_airtable_layer_id', api_airtable_layer.id)
