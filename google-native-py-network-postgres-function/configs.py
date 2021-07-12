b949561a8e8aa9f8dd1e9d2a3ac711b4845ede60
Tushar Shah <tushar.shah1@gmail.com>
2021-07-12 16:54:25 -0400

    added subnets and shorted names

--- google-native-py-network-postgres-function/configs.py
+++ google-native-py-network-postgres-function/configs.py
@@ -8,9 +8,13 @@ projectName = pulumi.get_project()
 # retrieving the stack name
 stackName = pulumi.get_stack()
 
+# retrieving vpc cidr blocks
+subnet_cidr_blocks = config.require_object('subnet_cidr_blocks')
+
+
 # Restriction on passing name with project that has google in it.
 def getResourceName(resourceName=""):
     if resourceName == "":
-        return f"{'pulumi-gcp-native'}"
+        return f"{'gcp-native'}"
     else:
-        return f"{'pulumi-gcp-native'}-{resourceName}"
+        return f"{'gcp-native'}-{resourceName}"
