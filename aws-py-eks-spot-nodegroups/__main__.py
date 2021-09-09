f2d9591b0683fe7537f9d40fc97663e48c71eaf8
Tushar Shah <tushar.shah1@gmail.com>
2021-09-09 16:56:27 -0400

    commented out managed node group, commented out the opts=Resourceoption since it was hanging the preview

--- aws-py-eks-spot-nodegroups/__main__.py
+++ aws-py-eks-spot-nodegroups/__main__.py
@@ -50,34 +50,32 @@ mycluster = eks.Cluster('demo-my-cluster',
         skip_default_node_group=True,
         instance_roles=[role1, role2],
         instance_type="t3a.medium",
+        node_root_volume_size= 10,
         encrypt_root_block_device=True,
         version="1.21",
         vpc_id= vpc.vpc.id,
         public_subnet_ids=vpc.public_subnet_ids,
         private_subnet_ids=vpc.private_subnet_ids,
-        enabled_cluster_log_types = [
-            'api',
-            'audit',
-            'authenticator',
-            'scheduler',
-            'controllerManager'
-        ]
+        enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"],
 )
 
-
+"""
 managed_node_group = eks.ManagedNodeGroup("demo-my-managed-ng",
                                            cluster=mycluster.core, # TODO[pulumi/pulumi-eks#483]: Pass cluster directly.
                                            disk_size=10,
                                            instance_types=["t3a.small"],
                                            scaling_config=aws.eks.NodeGroupScalingConfigArgs(
-                                              desired_size=2,
+                                              desired_size=1,
                                               min_size=1,
                                               max_size=3,
                                            ),
                                            capacity_type="SPOT",
                                            node_role=role1,
                                            tags={"env": "dev"},
-                                           opts=pulumi.ResourceOptions(parent=mycluster))
+                                           #opts=ResourceOptions(parent=mycluster),
+                                           #opts=pulumi.ResourceOptions(parent=mycluster)
+                                           )
+"""
 
 fixed_node_group = eks.NodeGroup('demo-my-ng-fixed',
                                  cluster=mycluster.core,
@@ -87,9 +85,8 @@ fixed_node_group = eks.NodeGroup('demo-my-ng-fixed',
                                  max_size=3,
                                  labels={'ondemand': 'true'},
                                  instance_profile=instance_profile_1,
-                                 opts=pulumi.ResourceOptions(parent=mycluster)
+                                 #opts=pulumi.ResourceOptions(parent=mycluster)
                                  )
-
 
 spot_node_group = eks.NodeGroup('demo-my-ng-spot',
                                 cluster=mycluster.core,
@@ -106,5 +103,5 @@ spot_node_group = eks.NodeGroup('demo-my-ng-spot',
                                     },
                                 },
                                 instance_profile=instance_profile_2,
-                                opts=pulumi.ResourceOptions(parent=mycluster)
+                                #opts=pulumi.ResourceOptions(parent=mycluster)
                                 )
