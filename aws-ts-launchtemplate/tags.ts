78e173d42a1cc5a01da78ed98842b1f8e3e058ff
Tushar Shah <tushar.shah1@gmail.com>
2021-09-24 10:38:21 -0400

    added tags.ts

---
+++ aws-ts-launchtemplate/tags.ts
@@ -0,0 +1,14 @@
+export interface Tag {
+  readonly Name: string;
+  readonly t_active: string;
+  readonly t_app: string;
+  readonly t_billing_center: string;
+  readonly t_autoScale: string;
+  readonly t_contact: string;
+  readonly t_deployment: string;
+  readonly t_env: string;
+  readonly t_owner: string;
+  readonly t_project: string;
+  readonly t_team: string;
+  readonly t_repository: string;
+}
