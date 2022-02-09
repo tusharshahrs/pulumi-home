d7fd0653327e8f174068be4ac0692350303661c1
Tushar Shah <tushar.shah1@gmail.com>
2022-02-09 15:22:29 -0500

    added aws-classic-ts-eks

---
+++ aws-classic-ts-eks/index.ts
@@ -0,0 +1,17 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as eks from "@pulumi/eks";
+import * as k8s from "@pulumi/kubernetes";
+import { Namespace } from "@pulumi/kubernetes/core/v1";
+
+const name = "demo"
+const mycluster = new eks.Cluster(`${name}-eks`, {
+    instanceType: "t3a.micro",
+    version: "1.21",
+    nodeRootVolumeSize: 10,
+    encryptRootBlockDevice: true, 
+});
+
+const awslb_namespace = new Namespace("awslb-controller-ns",{}, {provider: mycluster.provider});
+
+export const cluster_name = mycluster.eksCluster.name;
+export const kubeconfig = pulumi.secret(mycluster.kubeconfig);
