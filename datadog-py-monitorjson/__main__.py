e664257a4301d4bf4967dbb13d401cf873ccfb3c
Tushar Shah <tushar.shah1@gmail.com>
2022-03-02 13:44:29 -0500

    added no traffic monitorjson

--- datadog-py-monitorjson/__main__.py
+++ datadog-py-monitorjson/__main__.py
@@ -26,4 +26,29 @@ monitor_json = datadog.MonitorJson("monitorJson", monitor=json.dumps({
 })
 )
 
+mvr_no_traffic_cn = datadog.MonitorJson("NoTrafficMVRCN", monitor=json.dumps({
+    "name": "Example monitor - NoTrafficMVRCN",
+    "type": "service check",
+    "query": "\"ntp.in_sync\".by(\"*\").last(2).count_by_status()",
+    "message": "Change the message triggers if any host's clock goes out of sync with the time given by NTP. The offset threshold is configured in the Agent's 'ntp.yaml' file.\n\nSee [Troubleshooting NTP Offset issues](https://docs.datadoghq.com/agent/troubleshooting/ntp for more details on cause and resolution.",
+    "tags": [],
+    "multi": True,
+    "options": {
+        "include_tags": True,
+        "locked": False,
+        "new_host_delay": 150,
+        "notify_audit": False,
+        "notify_no_data": False,
+        "thresholds": {
+            "warning": 1,
+            "ok": 1,
+            "critical": 1
+        }
+    },
+    "priority": 2,
+    "classification": "custom"
+})
+)
+
 pulumi.export("monitor_json_name", monitor_json.id)
+pulumi.export("no_traffic_cn_name", mvr_no_traffic_cn.id)
