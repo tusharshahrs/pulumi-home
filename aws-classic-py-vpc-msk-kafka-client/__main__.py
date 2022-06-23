d4aca67a1c0e51c0e82c6296a77307a883ca97be
Tushar Shah <tushar.shah1@gmail.com>
2022-06-23 14:15:35 -0400

    added security group

--- aws-classic-py-vpc-msk-kafka-client/__main__.py
+++ aws-classic-py-vpc-msk-kafka-client/__main__.py
@@ -10,6 +10,8 @@ config = Config()
 my_vpc_cidr_block = config.get("vpc_cidr_block") or "10.0.0.0/25"
 my_number_of_nat_gateways_requested = config.get_int("number_of_nat_gateways") or 2
 my_number_of_availability_zones = config.get_int("number_of_availability_zones") or 2
+myip = config.get_secret("my_ipaddress");
+
 myname = "demo"
 
 myvpc = awsx.ec2.Vpc(f"{myname}-vpc",
@@ -22,3 +24,44 @@ export("vpc_id",myvpc.vpc_id)
 export("vpc_cidr_block", my_vpc_cidr_block)
 export("number_of_natgateways",my_number_of_nat_gateways_requested)
 export("availabililty_zones",my_number_of_availability_zones)
+export("public_subnets",myvpc.public_subnet_ids)
+export("private_subnets",myvpc.private_subnet_ids)
+
+## Security Group
+security_group = aws.ec2.SecurityGroup(
+    f"{myname}-security-group",
+    vpc_id=myvpc.id,
+    description='MSK Security Group for VPC',
+    tags={
+        'Name': f"{myname}-security-group",
+    },
+    ingress=[
+        aws.ec2.SecurityGroupIngressArgs(
+            cidr_blocks=[myip],
+            from_port=0,
+            to_port=0,
+            protocol='-1',
+            description='ingress rule for msk cluster for clients'
+        ),
+        aws.ec2.SecurityGroupIngressArgs(
+            cidr_blocks=[myip],
+            from_port=22,
+            to_port=22,
+            protocol='tcp',
+            description='my ssh rule for client server'
+        ),
+    ],
+    egress=[
+        aws.ec2.SecurityGroupEgressArgs(
+            cidr_blocks=["0.0.0.0/0"],
+            from_port=0,
+            to_port=0,
+            protocol="-1",
+            description="egress outbound rule for msk cluster"
+        )
+    ],
+    opts=ResourceOptions(parent=myvpc),
+) 
+
+export("security_group_name", security_group.name)
+export("security_group_id", security_group.id)
