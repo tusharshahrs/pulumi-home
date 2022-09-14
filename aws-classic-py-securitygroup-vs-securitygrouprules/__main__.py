90409ede2e9c5c19139a542cbec4be846cd85d9f
Tushar Shah <tushar.shah1@gmail.com>
2022-09-14 10:43:54 -0400

    updated the ports

--- aws-classic-py-securitygroup-vs-securitygrouprules/__main__.py
+++ aws-classic-py-securitygroup-vs-securitygrouprules/__main__.py
@@ -1,7 +1,7 @@
 """An AWS Python Pulumi program"""
 
 from cgitb import enable
-from pulumi import Config, export
+from pulumi import Config, export, ResourceOptions
 import pulumi_awsx as awsx
 from pulumi_aws import ec2
 
@@ -13,14 +13,17 @@ my_number_of_availability_zones = 3
 # variable for name
 myname = "demo"
 
-# VPC
-myvpc = awsx.ec2.Vpc(f"{myname}-vpc",
+# awsx vpc that is multilang
+myvpc = awsx.ec2.Vpc(f'{myname}-vpc',
     cidr_block= my_vpc_cidr_block,
     number_of_availability_zones = my_number_of_availability_zones,
     enable_dns_hostnames=True,
-    nat_gateways=awsx.ec2.vpc.NatGatewayConfigurationArgs(strategy =awsx.ec2.vpc.NatGatewayStrategy.SINGLE ),   
+    nat_gateways=awsx.ec2.vpc.NatGatewayConfigurationArgs(strategy =awsx.ec2.vpc.NatGatewayStrategy.SINGLE ), # To save cost, only do in dev.  In prod you would have 3 nats
     tags={
-        "Name":f"{myname}-vpc",
+        "Name":f'{myname}-vpc',
+        "awsx":"yes",
+        "console":"no",
+        "env":"dev"
       },
 )
 
@@ -34,17 +37,17 @@ security_group_no_sg_rules = ec2.SecurityGroup(f'{myname}-securitygroup',
     vpc_id=myvpc.vpc_id,
     tags={'Name': f'{myname}-securitygroup' },
     egress=[
-        ec2.SecurityGroupIngressArgs(
+        ec2.SecurityGroupEgressArgs(
             cidr_blocks=['0.0.0.0/0'],
             from_port=443,
             to_port=443,
             protocol='tcp',
-            description='Allow outbound access via https'
+            description='Allow outbound access via https',
         ),
-        ec2.SecurityGroupIngressArgs(
+        ec2.SecurityGroupEgressArgs(
             cidr_blocks=['0.0.0.0/0'],
-            from_port=80,
-            to_port=80,
+            from_port=80,  # After the 1st pulumi up, change the port to 81 to see only port change
+            to_port=80,    # After the 1st pulumi up, change the port to 81 to see only port change
             protocol='tcp',
             description='Allow outbound access via http'
         ),
@@ -59,7 +62,7 @@ security_group_no_sg_rules = ec2.SecurityGroup(f'{myname}-securitygroup',
         ),
         ec2.SecurityGroupIngressArgs(
             cidr_blocks=['0.0.0.0/0'],
-            from_port=80,
+            from_port=80, 
             to_port=80,
             protocol='tcp',
             description='Allow HTTP access inbound'
@@ -72,7 +75,7 @@ security_group_with_sg_rules = ec2.SecurityGroup(f'{myname}-securitygroup_with_s
     tags={'Name': f'{myname}-securitygroup_with_sg_rules' },
     )
 
-security_group_rule1 = ec2.SecurityGroupRule('{myname}-securitygrouprules1',
+security_group_rule1 = ec2.SecurityGroupRule(f'{myname}-securitygrouprules1',
     type="ingress",
     from_port=443,
     to_port=443,
@@ -81,10 +84,10 @@ security_group_rule1 = ec2.SecurityGroupRule('{myname}-securitygrouprules1',
     security_group_id=security_group_with_sg_rules.id,
 )
 
-security_group_rule2 = ec2.SecurityGroupRule('{myname}-securitygrouprules2',
+security_group_rule2 = ec2.SecurityGroupRule(f'{myname}-securitygrouprules2',
     type="egress",
-    from_port=81,
-    to_port=81,
+    from_port=81, # After the 1st pulumi up, change the port to 81 to see create-replace issue
+    to_port=81,   # After the 1st pulumi up, change the port to 81 to see create-replace issue
     protocol="tcp",
     cidr_blocks=['0.0.0.0/0'],
     security_group_id=security_group_with_sg_rules.id,
@@ -95,9 +98,9 @@ export("security_group_name", security_group_no_sg_rules.id)
 export("security_group_vpc", security_group_no_sg_rules.vpc_id)
 export("security_group_egress", security_group_no_sg_rules.egress)
 export("security_group_ingress", security_group_no_sg_rules.ingress)
+
 export("security_group_with_rules_name", security_group_with_sg_rules.id)
 export("security_group_with_rules_vpc", security_group_with_sg_rules.vpc_id)
-
 export("security_group_with_rules_egress", security_group_rule1.id)
 export("security_group_with_rules_ingress", security_group_rule2.id)
 
