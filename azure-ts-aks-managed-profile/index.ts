4e4ba12b3d334db057305dfeb8aa19ac13ceb9ee
Tushar Shah <tushar.shah1@gmail.com>
2022-10-19 15:45:06 -0400

    added toggle option

--- azure-ts-aks-managed-profile/index.ts
+++ azure-ts-aks-managed-profile/index.ts
@@ -65,8 +65,8 @@ const sshKey = new tls.PrivateKey(`${name}-ssh-key`, {
 
 const cluster = new containerservice.ManagedCluster(`${name}-managedcluster`, {
     resourceGroupName: resourceGroup.name,
-    aadProfile: {enableAzureRBAC: true, managed: true, tenantID: pulumi.interpolate`${current_tenantid}`},
-    //aadProfile: {enableAzureRBAC: false, managed: true, tenantID: pulumi.interpolate`${current_tenantid}`},adminGroupObjectIDs: ["mystuff"]},
+    aadProfile: {enableAzureRBAC: true, managed: true, tenantID: pulumi.interpolate`${current_tenantid}`}, // After 1st pulumi up, comment this out.
+    //aadProfile: {enableAzureRBAC: false, managed: true, tenantID: pulumi.interpolate`${current_tenantid}`, adminGroupObjectIDs: ["Test"]},  // Uncomment this and run pulumi up.  The preview shows create-replace of entire cluster.
     agentPoolProfiles: [{
         count: 3,
         maxPods: 11,
