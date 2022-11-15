2b51f31ea8d1849c327b837e9fd20b6c8ee09383
Tushar Shah <tushar.shah1@gmail.com>
2022-11-15 11:34:17 -0500

    added classic keyvault and access policy

--- azure-py-keyvault-appserviceplan-webservice/__main__.py
+++ azure-py-keyvault-appserviceplan-webservice/__main__.py
@@ -4,6 +4,8 @@ import pulumi
 from pulumi_azure_native import resources,keyvault, authorization
 from pulumi import export, Output
 from pulumi_azure_native.web.v20210201 import AppServicePlan, SkuDescriptionArgs, WebApp
+from pulumi_azure import keyvault as keyvault_classic
+import pulumi_azure as azure 
 
 name = "demo"
 
@@ -103,3 +105,25 @@ vault_1 = keyvault.Vault(f'{name}-keyvault',
 )
 
 export('vault_1_name', vault_1.name)
+
+keyvault_2_classic = keyvault_classic.KeyVault(f'{name}-keyvtclassic',
+    resource_group_name=resource_group.name,
+    sku_name="standard",
+    enabled_for_disk_encryption=True,
+    purge_protection_enabled=False,
+    tenant_id=config.tenant_id,
+    enabled_for_template_deployment=True,
+    opts=pulumi.ResourceOptions(depends_on=[resource_group])
+
+)
+keyvault_2_ =keyvault_classic.AccessPolicy(f'{name}-keyvtclassic-accesspolicy',
+        tenant_id=config.tenant_id,
+        object_id=config.object_id,
+        key_permissions=keys_var,
+        secret_permissions=secrets_var,
+        certificate_permissions=certificates_var,
+        key_vault_id=keyvault_2_classic.id,
+        opts=pulumi.ResourceOptions(depends_on=[resource_group,keyvault_2_classic])
+)
+
+export('vault_2_name', keyvault_2_classic.name)
