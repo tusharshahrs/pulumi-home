720023e33fe226620041613110cc1207e9d9cc0a
Tushar Shah <tushar.shah1@gmail.com>
2023-06-05 09:31:00 -0400

    working eks cluster with managed node and launch template

--- aws-classic-ts-eks-managednode-poddisruption/index.ts
+++ aws-classic-ts-eks-managednode-poddisruption/index.ts
@@ -4,11 +4,11 @@ import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
 import * as k8s from "@pulumi/kubernetes";
 
-const name = "shahtushar";
+const name = "tusharnolt";
 
 // Create a VPC with 3 public and 3 private subnets with the CIDR block 10.0.0.0/22.
 const vpc = new awsx.ec2.Vpc(`${name}-vpc`, {
-    cidrBlock: "10.0.0.0/24",
+    cidrBlock: "10.0.0.0/22",
     numberOfAvailabilityZones: 3,
     natGateways: { strategy: "Single"},
     tags: { "Name": `${name}-vpc` },
@@ -20,31 +20,39 @@ const cluster = new eks.Cluster(`${name}-eks`, {
     vpcId: vpc.vpcId,
     publicSubnetIds: vpc.publicSubnetIds,
     privateSubnetIds: vpc.privateSubnetIds,
-    //subnetIds: vpc.privateSubnetIds,
     skipDefaultNodeGroup: true,
     instanceType: "t2.small",
     desiredCapacity: 3,
     maxSize: 5,
-    version: "1.25",
+    version: "1.24",
     encryptRootBlockDevice: true,
     nodeRootVolumeSize: 10,
     enabledClusterLogTypes: ["api", "audit", "authenticator", "controllerManager", "scheduler", ],
     tags: { "Name": `${name}-eks` },
 });
 
+export const cluster_name = cluster.eksCluster.name;
+// Export the cluster's kubeconfig.
+export const kubeconfig = pulumi.secret(cluster.kubeconfig);
+export const mynodeRole = cluster.instanceRoles[0].arn;
+// Create a Kubernetes provider using the EKS cluster's kubeconfig.
+const k8sprovider = new k8s.Provider(`${name}-k8sprovider`, { kubeconfig });
+
 const managed_node_group = new eks.ManagedNodeGroup(
     `${name}-manangednodegroup`,
     {
       cluster: cluster,
-      capacityType: "SPOT",
+      //capacityType: "SPOT",
       instanceTypes: ["t3a.micro"],
-      nodeRole: cluster.instanceRoles.apply((roles) => roles[0]),
+      //nodeRole: cluster.instanceRoles.apply((roles) => roles[0]),
+      nodeRoleArn: cluster.instanceRoles[0].arn,
       //nodeRole: roles[0],
       labels: { managed: "true", spot: "true" },
       tags: {
+        "Name": `${name}-manangednodegroup`,
         "k8s.cluster-autoscaler/shaht-dev": "owned",
         "k8s.cluster-autoscaler/enabled": "True",
-        team: "rabbitdig",
+        team: "team-ce",
         environment: "development",
       },
   
@@ -55,7 +63,7 @@ const managed_node_group = new eks.ManagedNodeGroup(
       },
       diskSize: 10,
     },
-    { dependsOn: [vpc,cluster] }
+    { dependsOn: [vpc,cluster]}
   );
 
 export const managed_node_group_name = managed_node_group.nodeGroup.id;
@@ -63,13 +71,7 @@ export const managed_node_group_launchtemplate = managed_node_group.nodeGroup;
 export const managed_node_group_version =managed_node_group.nodeGroup.version;
 //export const managed_node_group_templateVersion = managed_node_group.nodeGroup.launchTemplateVersion;  
 
-
-// Export the cluster's kubeconfig.
-export const kubeconfig = cluster.kubeconfig;
-
-// Create a Kubernetes provider using the EKS cluster's kubeconfig.
-const k8sprovider = new k8s.Provider(`${name}-k8sprovider`, { kubeconfig });
-
+/*
 // Create a namespace using the eks cluster's kubeconfig
 const mynamespace = new k8s.core.v1.Namespace(
   `${name}-namespace`,
@@ -83,13 +85,18 @@ const pdb = new k8s.policy.v1.PodDisruptionBudget(`${name}-pdb`, {
     //metadata: { namespace: "default" },
     metadata: { namespace: mynamespace.metadata.name },
     spec: {
-        minAvailable: 1,
+      //minAvailable: "100%", // This is an error, minAvailable should be an integer or percentage less than 100.
+      maxUnavailable: "100%",
+        //minAvailable: 1,
         //maxUnavailable: 3,
         selector: {
             matchLabels: {
                 app: "myapp",
             },
         },
-        unhealthyPodEvictionPolicy: "NoEviction", // criteria for evicting unhealthy pods (NoEviction, StaticallyKubeletOnly)
+        //unhealthyPodEvictionPolicy: "NoEviction", // criteria for evicting unhealthy pods (NoEviction, StaticallyKubeletOnly)
     },
 }, { provider: k8sprovider });
+
+export const pdbName1 = pdb.metadata.name;
+*/
