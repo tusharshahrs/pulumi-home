9b413f5d039d2d68225926c66e170543d1fde676
Tushar Shah <tushar.shah1@gmail.com>
2024-01-31 15:16:51 -0500

    bumping monitoring chart to 0.9.1

--- azure-ts-aks-autoscale-grafana-autoscale-kubecost/index.ts
+++ azure-ts-aks-autoscale-grafana-autoscale-kubecost/index.ts
@@ -277,7 +277,7 @@ export const namespace_metrics = metrics_namespace.metadata.name;
 
 const prometheusmetrics_k8s_monitoring = new k8s.helm.v3.Release(`${name}-k8smonitoringhelmr`, {
     chart: "k8s-monitoring",
-    version: "0.9.0",
+    version: "0.9.1",
     namespace: metrics_namespace.metadata.name,
     repositoryOpts: {
         repo: "https://grafana.github.io/helm-charts",
