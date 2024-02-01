282613ce6f74ff4a97347e7c8a01450ea342c9d4
Tushar Shah <tushar.shah1@gmail.com>
2024-02-01 09:57:38 -0500

    bumped kubecost helmchart

--- aws-classic-ts-vpc-eks2-spot-nodegroup/index.ts
+++ aws-classic-ts-vpc-eks2-spot-nodegroup/index.ts
@@ -317,7 +317,7 @@ export const namespace_kubecost = kubecost_namespace.metadata.name;
 // https://github.com/kubecost/cost-analyzer-helm-chart
 const kubecostchart = new k8s.helm.v3.Release(`${name}-kubecosthelmr`, {
   chart: "cost-analyzer",
-  version: "2.0.0",
+  version: "2.0.1",
   namespace: kubecost_namespace.metadata.name,
   repositoryOpts: {
       repo: "https://kubecost.github.io/cost-analyzer/",
