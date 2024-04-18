322bfb1b061806ea259d89d526f3e30aed01f82c
Tushar Shah <tushar.shah1@gmail.com>
2024-04-18 15:23:06 -0400

    bumped the helm chart versions

--- aws-classic-ts-vpc-eks2-spot-nodegroup/index.ts
+++ aws-classic-ts-vpc-eks2-spot-nodegroup/index.ts
@@ -348,7 +348,7 @@ export const namespace_grafana_k8s_monitoring = grafana_k8s_monitoring_namespace
 // https://github.com/grafana/k8s-monitoring-helm/tree/main/charts/k8s-monitoring
 const grafana_k8s_monitoring = new k8s.helm.v3.Release(`${name}-k8smonitoring-helm`, {
   chart: "k8s-monitoring",
-  version: "0.13.1",
+  version: "1.0.1",
   //chart: "prometheus",
   //version: "25.11.0",
   namespace: grafana_k8s_monitoring_namespace.metadata.name,
@@ -468,7 +468,7 @@ export const namespace_kubecost = kubecost_namespace.metadata.name;
 // https://github.com/kubecost/cost-analyzer-helm-chart
 const kubecostchart = new k8s.helm.v3.Release(`${name}-kubecost-helm`, {
   chart: "cost-analyzer",
-  version: "2.2.1",
+  version: "2.2.2",
   namespace: kubecost_namespace.metadata.name,
   repositoryOpts: {
       repo: "https://kubecost.github.io/cost-analyzer/",
